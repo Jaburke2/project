317e031ff8969cb0ca0aef197720fc789ec78d2a
Joshua Burke <Jaburke2@buffs.wtamu.edu>
2022-11-30 22:04:47 -0600

    fe

--- final_project/urls.py
+++ final_project/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("accounts/", include('django.contrib.auth.urls')),
     path("accounts/", include("accounts.urls")),
     path("", include("blog.urls")),
-] + + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
