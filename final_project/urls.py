5ee29e3292a4800f1c3fc2dff72d7e73a6769416
Joshua Burke <Jaburke2@buffs.wtamu.edu>
2022-11-30 22:03:03 -0600

    fkn

--- final_project/urls.py
+++ final_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include('django.contrib.auth.urls')),
     path("accounts/", include("accounts.urls")),
     path("", include("blog.urls")),
-]
+] + + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
