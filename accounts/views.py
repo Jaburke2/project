2ec03dbab403ebed280cc50e55c286410f37a375
Joshua Burke <Jaburke2@buffs.wtamu.edu>
2022-11-30 21:10:33 -0600

    oh no

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
-class HomePageView(TemplateView):
-    template_name = "home.html"
 
-class AboutPageView(TemplateView):
-    template_name = "about.html"
-    
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 
